740a53a721e2154cac79b2f06b57ec0ef8f35fc5
sqine <sqine7@gmail.com>
2021-11-30 00:13:06 +0900

    default version of vue project applied sass

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "@/assets/styles/_default.scss";
+
+#app {
+  background: #{$bg-color};
+  font-family: #{$font-family-default}, sans-serif;
+  @include fontSet;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    Home
+    <span class="material-icons-outlined"> open_in_new </span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
